ce7ecd88348c0bb332880bdc5b44dc26a1bbb54e
Tomy Barranco <50777025+tomy-barranco@users.noreply.github.com>
2025-04-02 23:20:40 -0300

    Delete modules/guardar_agenda.py

--- modules/guardar_agenda.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-def guardar_agenda_csv(agenda, archivo="agenda.csv"):
-    with open(archivo, "w", newline="", encoding="utf-8") as f:
-        writer = csv.DictWriter(f, fieldnames=["nombre", "telefono", "correo"])
-        writer.writeheader()  # Escribe la primera fila con los nombres de las columnas
-        writer.writerows(agenda)
